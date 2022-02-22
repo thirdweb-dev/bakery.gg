8cc5830a68de666b9c9aed83167ca6a36f365b48
Jake Loo <2171134+jakeloo@users.noreply.github.com>
2022-02-22 22:08:04 +0000

    usebakery, fix build

--- src/hooks/useBakery.ts
+++ src/hooks/useBakery.ts
@@ -1,5 +1,6 @@
-import { useNetwork, useSigner } from "@thirdweb-dev/react";
-import { useCallback, useMemo } from "react";
+import { useNetwork, useSigner, useAddress } from "@thirdweb-dev/react";
+import { BigNumber } from "ethers";
+import { useEffect, useMemo, useState } from "react";
 import { Bakery__factory } from "../../types/ethers-contracts";
 import { ChainId } from "../utils/network";
 
@@ -9,7 +10,13 @@ const CONTRACT_ADDRESSES: Record<number, string> = {
 
 export function useBakery() {
   const signer = useSigner();
+  const signerAddress = useAddress();
   const [network] = useNetwork();
+  const [cookiePerSecond, setCookiePerSecond] = useState(0);
+  const [cookiePerClick, setCookiePerClick] = useState(0);
+  const [bakeStartBlock, setBakeStartBlock] = useState(0);
+  const [maxNumberOfBlockReward, setMaxNumberOfBlockReward] = useState(0);
+  const [isBaking, setIsBaking] = useState(false);
 
   const contract = useMemo(() => {
     if (!network.data) {
@@ -20,9 +27,50 @@ export function useBakery() {
     }
     const chainId = network.data.chain?.id || ChainId.Mumbai;
     return Bakery__factory.connect(CONTRACT_ADDRESSES[chainId], signer.data);
-  }, [network, signer]);
+  }, [network.data, signer.data]);
+
+  useEffect(() => {
+    async function update() {
+      const rewardPerBlock =
+        (await contract?.rewardPerBlock()) ?? BigNumber.from(0);
+
+      // TODO convert perBlock to perSecond
+      setCookiePerSecond(rewardPerBlock.toNumber());
+      // TODO get cookie per click
+      setCookiePerClick(1.0);
+
+      // eslint-disable-next-line
+      const maxReward = await contract?.MAX_NUMBER_OF_BLOCK_FOR_REWARD();
+      setMaxNumberOfBlockReward(maxReward?.toNumber() ?? 0);
+
+      if (signerAddress) {
+        const oven = await contract?.ovens(signerAddress);
+        setIsBaking(oven?.startBlock.gt(0) ?? false);
+        setBakeStartBlock(oven?.startBlock.toNumber() ?? 0);
+      }
+    }
+    if (contract) {
+      update();
+    }
+  }, [contract, signerAddress]);
+
+  const isExceedMaxBakeLimit = useMemo(async () => {
+    if (!signer?.data || !maxNumberOfBlockReward || !bakeStartBlock) {
+      return false;
+    }
+    const currentBlockNumber = await signer.data?.provider?.getBlockNumber();
+    if (!currentBlockNumber) {
+      return false;
+    }
+    return currentBlockNumber - bakeStartBlock > maxNumberOfBlockReward;
+  }, [signer.data, maxNumberOfBlockReward, bakeStartBlock]);
 
   return {
     contract,
+    cookiePerClick,
+    cookiePerSecond,
+    bakeStartBlock,
+    isBaking,
+    isExceedMaxBakeLimit,
   };
 }
